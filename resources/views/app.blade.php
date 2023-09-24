852d264a5b74c5e0af0d1642d7328e0cc02b9426
uzairahmed <uzii.ahmed1643@gmail.com>
2023-09-24 14:33:37 +0500

    Project Initialization

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HR | Attendance Module</title>
+
+    {{ vite_assets() }}
+</head>
+<body>
+	<div id="root"></div>
+</body>
+</html>
